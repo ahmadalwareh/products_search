6bac564eb6c64e633585b43eb12acf707bf60aec
Ahmad Alwareh <ahmadalwareh@outlook.com>
2023-06-30 01:11:48 +0300

    Fixed nested Dirs adding

--- product_search/products/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import ProductSearchView
-
-app_name = 'products'
-
-urlpatterns = [
-    path('search/', ProductSearchView.as_view(), name='product_search'),
-
-]
